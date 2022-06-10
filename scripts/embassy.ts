7b626b25f56984e460b0316602b488249c8283ca
J M <mogulslayer@gmail.com>
2022-06-10 14:07:21 -0600

    fix: Latest types

--- scripts/embassy.ts
+++ scripts/embassy.ts
@@ -10,7 +10,8 @@ import {
   SetResult,
   Properties,
   Dependencies,
-} from "https://raw.githubusercontent.com/Start9Labs/embassy-os/master/backend/test/js_action_execute/package-data/scripts/test-package/0.3.0.3/types.d.ts";
+  ExpectedExports
+} from "https://raw.githubusercontent.com/Start9Labs/embassy-os/5a88f41718836e2b05a0b67c096193e0070fb4ec/libs/artifacts/types.d.ts";
 
 const matchesStringRec = some(
   string,
@@ -32,7 +33,7 @@ const matchesConfigFile = shape({
   password: string,
 });
 
-export async function getConfig(effects: Effects): Promise<ConfigRes> {
+export const getConfig: ExpectedExports.getConfig = async (effects: Effects) => {
   const config = await effects
     .readJsonFile({
       volumeId: "main",
@@ -42,43 +43,45 @@ export async function getConfig(effects: Effects): Promise<ConfigRes> {
     .catch(() => undefined);
 
   return {
-    config,
-    spec: {
-      "tor-address": {
-        name: "Tor Address",
-        description: "The Tor address.",
-        type: "pointer",
-        subtype: "package",
-        "package-id": "syncthing",
-        target: "tor-address",
-        interface: "main",
-      },
-      username: {
-        type: "string",
-        name: "Username",
-        description: "The user for loging into the administration page of syncthing",
-        nullable: false,
-        copyable: true,
-        masked: false,
-        default: "admin",
-      },
-      password: {
-        type: "string",
-        name: "Password",
-        description: "The password for loging into the administration page of syncthing",
-        nullable: false,
-        copyable: true,
-        masked: true,
-        default: {
-          charset: "a-z,A-Z,0-9",
-          len: 22,
+    result: {
+      config,
+      spec: {
+        "tor-address": {
+          name: "Tor Address",
+          description: "The Tor address.",
+          type: "pointer",
+          subtype: "package",
+          "package-id": "syncthing",
+          target: "tor-address",
+          interface: "main",
+        },
+        username: {
+          type: "string",
+          name: "Username",
+          description: "The user for loging into the administration page of syncthing",
+          nullable: false,
+          copyable: true,
+          masked: false,
+          default: "admin",
+        },
+        password: {
+          type: "string",
+          name: "Password",
+          description: "The password for loging into the administration page of syncthing",
+          nullable: false,
+          copyable: true,
+          masked: true,
+          default: {
+            charset: "a-z,A-Z,0-9",
+            len: 22,
+          },
         },
       },
-    },
+    }
   };
 }
 
-export async function setConfig(effects: Effects, input: Config): Promise<SetResult> {
+export const setConfig: ExpectedExports.setConfig = async (effects: Effects, input: Config) => {
   await effects.writeJsonFile({
     path: "./config.json",
     toWrite: {
@@ -88,16 +91,18 @@ export async function setConfig(effects: Effects, input: Config): Promise<SetRes
     volumeId: "main",
   });
   return {
-    signal: "SIGTERM",
-    "depends-on": {},
-  };
+    result: {
+      signal: "SIGTERM",
+      "depends-on": {},
+    }
+  }
 }
 
 const matchesSyncthingSystem = shape({
   myID: string,
 });
 
-export async function properties(effects: Effects): Promise<Properties> {
+export const properties: ExpectedExports.properties = async (effects) => {
   const syncthing_system_promise = effects.readJsonFile({
     volumeId: "main",
     path: "syncthing_stats.json",
@@ -110,7 +115,7 @@ export async function properties(effects: Effects): Promise<Properties> {
   const syncthing_system = matchesSyncthingSystem.unsafeCast(await syncthing_system_promise);
   const config = matchesConfigFile.unsafeCast(await config_promise);
 
-  return {
+  const result: Properties = {
     version: 2,
     data: {
       "Device Id": {
@@ -139,4 +144,5 @@ export async function properties(effects: Effects): Promise<Properties> {
       },
     },
   };
+  return { result };
 }
