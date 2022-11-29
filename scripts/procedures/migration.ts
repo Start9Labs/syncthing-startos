eab5a76e94ac1f7b6a4944790c371dc3c8c0825c
BluJ <mogulslayer@gmail.com>
2022-11-28 19:13:45 -0700

    chore: Add a migration?

--- scripts/procedures/migration.ts
+++ scripts/procedures/migration.ts
@@ -1,4 +1,22 @@
-import { compat, types as T } from "../deps.ts";
+import { compat, matches, types as T } from "../deps.ts";
 
 export const migration: T.ExpectedExports.migration = compat.migrations
-  .fromMapping({}, "1.22.0");
+  .fromMapping({
+    "1.22.1.1":
+    {
+      up: compat.migrations.updateConfig(
+        (config) => {
+          return config;
+        },
+        false,
+        { version: "1.22.1.1", type: "up" },
+      ),
+      down: compat.migrations.updateConfig(
+        (config) => {
+          return config;
+        },
+        false,
+        { version: "1.22.1.1", type: "down" },
+      ),
+    }
+  }, "1.22.1.1");
