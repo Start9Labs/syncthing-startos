1e4e3b1b285549b847e9e921a80a8c971d213c2c
BluJ <mogulslayer@gmail.com>
2023-01-16 15:46:26 -0700

    chore: Use js instead of compat

--- scripts/embassy.ts
+++ scripts/embassy.ts
@@ -3,3 +3,4 @@ export { setConfig } from "./procedures/setConfig.ts";
 export { properties } from "./procedures/properties.ts";
 export { health } from "./procedures/health.ts";
 export { migration } from "./procedures/migration.ts";
+export { createBackup, restoreBackup } from "./procedures/backups.ts";
